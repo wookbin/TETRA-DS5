6db7bac2278a88b6a90222aedfc4338bbcdea004
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2022-11-01 09:40:35 +0900

    Update tetraDS_service.cpp

--- tetraDS_service/src/tetraDS_service.cpp
+++ tetraDS_service/src/tetraDS_service.cpp
@@ -399,8 +399,6 @@ std_msgs::Int32 docking_progress;
 ros::Publisher virtual_obstacle_pub;
 //custom_msgs::Obstacles virtual_obstacle;
 virtual_costmap_layer::Obstacles virtual_obstacle;
-//Compare msg data
-virtual_costmap_layer::Obstacles old_virtual_obstacle;
 //Virtual Costmap2//
 ros::Publisher virtual_obstacle2_pub;
 //custom_msgs::Obstacles virtual_obstacle2;
@@ -1771,45 +1769,6 @@ bool Virtual_Obstacle_Command(tetraDS_service::virtual_obstacle::Request &req, t
 	bool bResult = false;
 	int m_iInt_count = 0;
 	int m_iNext_count = 0;
-	int m_icompare_count = 0;
-
-	//array_compare_check ////////////////////////////////////////////////////////////////////////////////////////////////////  
-	if(old_virtual_obstacle.list.size() == req.list_count.size())
-	{
-		//printf("Same Data_list count!!!\n");
-		for(int i=0; i<req.list_count.size(); i++)
-		{
-		    m_iInt_count = req.list_count[i];
-		    for(int j=0; j<m_iInt_count; j++)
-		    {
-			if(old_virtual_obstacle.list[i].form[j].x != floor(req.form_x[m_iNext_count + j] * 1000.f + 0.5) / 1000.f)
-			{   
-			    m_icompare_count++;
-			}
-			if(old_virtual_obstacle.list[i].form[j].y != floor(req.form_y[m_iNext_count + j] * 1000.f + 0.5) / 1000.f)
-			{
-			    m_icompare_count++;
-			}
-			if(old_virtual_obstacle.list[i].form[j].z != floor(req.form_z[m_iNext_count + j] * 1000.f + 0.5) / 1000.f)
-			{
-			    m_icompare_count++;
-			}
-
-		    }
-		    m_iNext_count += m_iInt_count;
-
-		}
-		//printf("m_icompare_count: %d \n", m_icompare_count);
-		if(m_icompare_count == 0)
-		{
-		    //printf("Skip Virtual costmap!! \n");
-		    return true;
-		}
-	}
-
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	m_iInt_count = 0;
-	m_iNext_count = 0;
 
 	// msg clear
 	virtual_obstacle.list.clear();
@@ -1829,11 +1788,8 @@ bool Virtual_Obstacle_Command(tetraDS_service::virtual_obstacle::Request &req, t
 	m_iNext_count += m_iInt_count;
 
 	}
-    
+	
 	virtual_obstacle_pub.publish(virtual_obstacle);
-
-	//copy
-	old_virtual_obstacle = virtual_obstacle;
  
 	/*
 	int32[]  list_count
