959a034d4c1386771aa0911d17c19a851d063e0b
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2022-12-23 10:25:47 +0900

    Update dssp_rs232_drive_module.h

--- tetraDS/src/dssp_rs232_drive_module.h
+++ tetraDS/src/dssp_rs232_drive_module.h
@@ -1,9 +1,8 @@
 
-
 int  dssp_rs232_drv_module_create(const char *device, int time_out);
 void dssp_rs232_drv_module_destroy();
 int  dssp_rs232_drv_module_read_encoder(int *encoder_l, int *encoder_r);	
-int  dssp_rs232_drv_module_read_bumper_emg(int *bumper_data, int *emg_state);
+int  dssp_rs232_drv_module_read_bumper_emg(int *bumper_data, int *emg_state, int *left_error_code, int *right_error_code);
 int  dssp_rs232_drv_module_read_drive_err(unsigned char *left_wheel_err, unsigned char *right_wheel_err);
 int  dssp_rs232_drv_module_read_parameter(int param, int *value);
 int  dssp_rs232_drv_module_set_servo(int mode);
